d0bec50f633728d0dbb0eb21dad8f4d2fc469080
AhmadNurHidayatullah <127074146+AhmadNurHidayatullah@users.noreply.github.com>
2023-04-13 14:45:39 +0700

    void input

--- LinearSearch_135/LinearSearch_135.cpp
+++ LinearSearch_135/LinearSearch_135.cpp
@@ -23,7 +23,10 @@ void input()
     cout << " Enter array element \n";
     cout << "\n----------------------\n";
     for (i = 0; i < n; i++)
-
+    {
+        cout << "<" << (i + 1) << "> ";
+        cin >> arr[i];
+    }
 }
 int main()
 {
